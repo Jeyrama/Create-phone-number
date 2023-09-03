70be872937bf95ef9532f3cf9d97910c700cfa47
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-02 17:19:00 -0700

    Update Create-phone-number.js

--- Create-phone-number.js
+++ Create-phone-number.js
@@ -19,3 +19,11 @@ function createPhoneNumber(numbers) {
 }
 
 // or
+
+function createPhoneNumber(numbers) {
+  numbers = numbers.join('');
+  return '(' + numbers.substring(0, 3) + ') '
+    + numbers.substring(3, 6)
+    + '-'
+    + numbers.substring(6);
+}
